d4dfe3dd65d26a48d4187e918dd26f4984aa0e24
Karthik <karthikkssuresh@gmail.com>
2021-10-20 03:33:09 +0530

    added proctfile and gitignore

--- app.js
+++ app.js
@@ -97,7 +97,11 @@ const requestedPostId = req.params.postName;
 });
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
